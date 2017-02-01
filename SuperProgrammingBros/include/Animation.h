51b53e63ea3e060f75f67bc3b44de0c1c8e6ca00
dosterz97 <dosterz97@gmail.com>
2017-02-01 09:15:15 -0600

    I shouldnt do this

--- SuperProgrammingBros/include/Animation.h
+++ SuperProgrammingBros/include/Animation.h
@@ -1,8 +1,24 @@
 #pragma once
+#include <string>
+#include "SDL.h"
+
 class Animation
 {
 public:
+	Animation(std::string, bool, SDL_Renderer*);
 	Animation();
 	~Animation();
+	SDL_Surface* getSurface();
+	void setStill(bool);
+	bool getStill();
+	void loadSurface(std::string);
+	std::string getName();
+	
+private:
+	std::string name;
+	bool isStill;
+	SDL_Renderer* renderer;
+	SDL_Surface* surface;
+	SDL_Texture* texture;
+	void createTexture();
 };
-
